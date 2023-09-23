6b6f6ba2004600346537c8c5bf1c9c7282fee4ed
Asus <anuradh-se19049@stu.kln.ac.lk>
2023-09-23 22:24:20 +0530

    Add properties to search icon

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './WeatherApp.css'
 import search from '../images/image1.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons"; // Import the faSearch icon
+import { faSearch } from "@fortawesome/free-solid-svg-icons"; 
 
 export const WeatherApp = () => {
   return (
@@ -10,7 +10,8 @@ export const WeatherApp = () => {
         <div className="top-bar">
             <input type="text" className="cityInput" placeholder='Search City'/>
             <div className="search-icon">
-                <FontAwesomeIcon icon={faSearch} /> {<FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />}
+                <FontAwesomeIcon icon={faSearch} className='custom-icon'/> 
+                {<FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />}
             </div>
         </div>
     </div>
